ab8236b7de0e5f21c242aabdf370a2e93391018a
Rahman Duran <a.rahman.duran@gmail.com>
2012-09-10 08:41:43 +0300

    implement multiprocessing for device polling

--- device_tracker.py
+++ device_tracker.py
@@ -2,20 +2,31 @@
 import yaml
 import inspect
 import time
+from multiprocessing import Process
 from plugin_factory import PluginFactory
+
+
+def runProcess(device_type, output_type, device_ip, community):
+  print dev + " - " + device_type
+  device = factory.getDevicePlugin(device_type, device_ip, community)
+  output = factory.getOutputPlugin(output_type)
+  output.output(device.getL2Data())
 
 f = open('config.yml', 'r')
 config = yaml.load(f)
 print config
 f.close()
 
+
 factory = PluginFactory()
+processes = {}
 print time.time()
 for dev, options in config["devices"].items():
-  print dev + " - " + options["type"]
-  device = factory.getDevicePlugin(options["type"], dev, options["community"])
-  output = factory.getOutputPlugin('syslogger')
-  output.output(device.getL2Data())
+  processes[dev] = Process(target=runProcess, args=(options["device"], options["output"], dev, options["community"]))
+  processes[dev].start()
+for dev, process in processes.items():
+  process.join()
+
 print time.time()
 
 
