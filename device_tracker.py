2d24d756892c58533fd45a3dfc26450467a2bce8
Rahman Duran <a.rahman.duran@gmail.com>
2012-09-10 16:01:05 +0300

    fix layers config loading

--- device_tracker.py
+++ device_tracker.py
@@ -12,9 +12,9 @@ def runProcess(device_type, output_type, device_layers, device_ip, community):
   output = factory.getOutputPlugin(output_type)
   for l in device_layers:
     if l.lower() == "l2":
-      output.output(device.getL2Data())
+      output.output(device.getL2Data(), l)
     if l.lower() == "l3":
-      output.output(device.getL3Data())
+      output.output(device.getL3Data(), l)
 
 f = open('config.yml', 'r')
 config = yaml.load(f)
@@ -26,7 +26,7 @@ factory = PluginFactory()
 processes = {}
 print time.time()
 for dev, options in config["devices"].items():
-  processes[dev] = Process(target=runProcess, args=(options["device"], options["output"], options["layer"], dev, options["community"]))
+  processes[dev] = Process(target=runProcess, args=(options["device"], options["output"], options["layers"], dev, options["community"]))
   processes[dev].start()
 for dev, process in processes.items():
   process.join()
