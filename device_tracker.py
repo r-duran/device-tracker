d03555b88f1dbb5d8c89f1c46d2501357db80d9a
Rahman Duran <a.rahman.duran@gmail.com>
2012-09-17 15:37:49 +0300

    fix multiprocessing problems

--- device_tracker.py
+++ device_tracker.py
@@ -2,20 +2,28 @@
 import yaml
 import inspect
 import time
-import sys
-from multiprocessing import Process
+import sys, traceback
+from multiprocessing import Process, Queue
 from plugin_factory import PluginFactory
 
 
 
-def runProcess(output_queue, device_type, output_type, device_layers, device_ip, community):
-  print dev + " - " + device_type
+def runProcess(q, device_type, output_type, device_layers, device_ip, community):
   device = factory.getDevicePlugin(device_type, device_ip, community)
   for l in device_layers:
     if l.lower() == "l2":
-      output_queue[device_ip] = {"data":device.getL2Data(), "type":output_type}
+      try:
+        q.put({"data":device.getL2Data(), "type":output_type, "device":device_ip})
+      except:
+        print "Cant put result to queue"
+        print traceback.format_exc()
     if l.lower() == "l3":
-      output_queue[device_ip] = {"data":device.getL3Data(), "type":output_type}
+      try:
+        q.put({"data":device.getL3Data(), "type":output_type, "device":device_ip})
+      except:
+        print "Cant put result to queue"
+        print traceback.format_exc()
+  q.put({"status":"DONE", "device":device_ip})
 
 if __name__ == "__main__":
   if len(sys.argv) < 3:
@@ -27,24 +35,31 @@ if __name__ == "__main__":
 
   f = open(sys.argv[2], 'r')
   config = yaml.load(f)
-  print config
   f.close()
 
 
   factory = PluginFactory(config["plugins_dir"])
   processes = {}
-  output_data = {}
+  output_data = Queue()
   print time.time()
   for dev, options in config["devices"].items():
     processes[dev] = Process(target=runProcess, args=(output_data, options["device"], options["output"], options["layers"], dev, options["community"]))
     processes[dev].start()
-  for dev, process in processes.items():
-    process.join()
-  for k, v in output_data.items():
-    output = factory.getOutputPlugin(v["type"])
-    output.output(v[data])
+  while True:
+    try:
+      if len(processes) == 0:
+        break
+      data = output_data.get()
+      if "status" in data:
+        processes[data["device"]].join()
+        del processes[data["device"]]
+      else:
+        output = factory.getOutputPlugin(data["type"])
+        output.output(data["data"])
+    except:
+      print "Queue read error"
+      print traceback.format_exc()
+      break
 
   print time.time()
-
-
 
